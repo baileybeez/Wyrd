1e3e333a3af934b2c4d08598e67f1715e28b053b
BaileyBeez <2171091+baileybeez@users.noreply.github.com>
2026-07-11 16:18:49 -0400

    added memset, u64 support to kvprintf

---
+++ src/kernel/lib/mem.c
@@ -0,0 +1,12 @@
+#include "bee.h"
+#include "mem.h"
+
+void* memset(void* dest, u8 val, u32 count)
+{
+   u8* ptr = (u8*)dest;
+   u8* end = ptr + count;
+   for (; ptr < end; ptr++) {
+      *ptr = val;
+   }
+   return dest;
+}
---
+++ src/kernel/lib/mem.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "bee.h"
+
+void* memset(void* dest, u8 val, u32 count);
