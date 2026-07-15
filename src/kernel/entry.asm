ab5b62165b966d3a6718617d141586d5b1ad63cc
BaileyBeez <2171091+baileybeez@users.noreply.github.com>
2026-07-14 21:37:07 -0400

    allow entry point (asm) to setup simple paging before calling kernel

--- src/kernel/entry.asm
+++ src/kernel/entry.asm
@@ -1,5 +1,8 @@
 ; Multiboot2 header + entry stub.
-; Sets up a stack and jumps into C (kernelMain).
+; 
+; bootstrap a temporary identity and higher-half 4MB page, 
+; enable paging, and jump into the higher-half before calling
+; into kernelMain
 
 section .multiboot
 align 8
@@ -15,19 +18,60 @@ header_start:
    dd 8
 header_end:
 
-section .bss
+kKernelVirtualBase      equ 0xC0000000
+kPageDirEntryIdentity   equ 0
+kPageDirEntryHighHalf   equ (kKernelVirtualBase >> 22)
+kPdeFlags               equ 0x83                         ; present | writable | PS (4MB)
+
+section .boot_bss nobits alloc noexec write align=4096
+global boot_page_directory
+boot_page_directory:
+   resd  1024
+
 align 16
-stack_bottom:
-   resb 16384                             ; 16 KiB kernel stack
-stack_top:
+boot_stack_bottom:
+   resb  4096
+boot_stack_top:
+
+section .boot_text progbits alloc exec nowrite align=16
+global _start
+_start:
+   cld 
+
+   mov   esp, boot_stack_top
+   push  ebx                              ; multiboot info pointer
+   push  eax                              ; multiboot magic number
+
+   mov   edi, boot_page_directory
+   xor   eax, eax
+   mov   ecx, 1024
+   rep   stosd                            ; zero the page directory
+
+   mov   dword [boot_page_directory + kPageDirEntryIdentity * 4], kPdeFlags
+   mov   dword [boot_page_directory + kPageDirEntryHighHalf * 4], kPdeFlags
+
+   mov   eax, boot_page_directory
+   mov   cr3, eax
+
+   mov   eax, cr4 
+   or    eax, 0x00000010                  ; CR4.PSE
+   mov   cr4, eax
+   
+   mov   eax, cr0
+   or    eax, 0x80000000                  ; CR0.PG 
+   mov   cr0, eax
+
+   jmp   higher_half_entry
 
 section .text
-global _start
 extern kernelMain
-_start:
-   mov   esp, stack_top
-   push  ebx                              ; multiboot info pointer
-   push  eax                              ; multiboot magic
+higher_half_entry:
+   pop   eax                              ; restore multiboot magic
+   pop   ebx                              ; restore multiboot info pointer
+
+   mov   esp, stack_top                   ; setup real stack
+   push  ebx
+   push  eax
    cld
    call  kernelMain
 
@@ -35,3 +79,9 @@ _start:
    cli
    hlt
    jmp .hang
+
+section .bss
+align 16
+stack_bottom:
+   resb 16384                             ; 16 KiB kernel stack
+stack_top:
