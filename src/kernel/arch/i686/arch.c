da14df300ad9a4435214c16dbeea47efd78bfb00
BaileyBeez <2171091+baileybeez@users.noreply.github.com>
2026-07-25 16:39:52 -0400

    mirgrated arch related init calls out of kernelMain

---
+++ src/kernel/arch/i686/arch.c
@@ -0,0 +1,33 @@
+#include "wyrd.h"
+#include "arch.h"
+#include "gdt.h"
+#include "idt.h"
+#include "irq.h"
+#include "pic.h"
+#include "syscallEntry.h"
+#include "ticks.h"
+#include "tss.h"
+#include "lib/logger.h"
+
+void archInitEarly()
+{
+   gdtInit();
+   kTrace("+ GDT initialized.");
+   tssInit();
+   kTrace("+ TSS initialized.");
+   idtInit();
+   kTrace("+ IDT initialized.");
+   picRemap(kIrqBase, kIrqBase + 8);
+   irqInit();
+}
+
+void archInitLate()
+{
+   ticksInit(100);
+   picClearMask(0);
+}
+
+void archEnableInterrupts()
+{
+   __asm__ volatile("sti");
+}
---
+++ src/kernel/arch/i686/arch.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "wyrd.h"
+
+void archInitEarly();
+void archInitLate();
+void archEnableInterrupts();
