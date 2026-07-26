0f07081b933af4327c1051a6a4ab056b279cf666
BaileyBeez <2171091+baileybeez@users.noreply.github.com>
2026-07-26 00:08:35 -0400

    introduced memcpy

--- src/kernel/lib/mem.c
+++ src/kernel/lib/mem.c
@@ -10,3 +10,14 @@ void* memset(void* dest, u8 val, u32 count)
    }
    return dest;
 }
+
+// TODO: we'll want to migrate this to a word-size copy with remnant handling
+void* memcpy(void* dest, const void* src, u32 count)
+{
+   u8* p       = (u8*)dest;
+   const u8* s = (const u8*)src;
+   while(count--)
+      *p++ = *s++;
+   
+   return dest;
+}
