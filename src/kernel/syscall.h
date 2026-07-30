10a347ea8cfedb8c33d9a47791e81f0c106ecb80
BaileyBeez <2171091+baileybeez@users.noreply.github.com>
2026-07-30 12:45:42 -0400

    introduced sysexit syscall

--- src/kernel/syscall.h
+++ src/kernel/syscall.h
@@ -3,7 +3,8 @@
 
 #define kSys_Read       0
 #define kSys_Write      1
+#define kSys_Exit       2
 
-#define kSyscallCount   2
+#define kSyscallCount   3
 
 i32  syscallDispatch(u32 num, u32 a0, u32 a1, u32 a2);
