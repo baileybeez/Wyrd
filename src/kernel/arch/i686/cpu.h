5e502e4d237e8197dc8484ffd0ef8f675a4c57cd
BaileyBeez <2171091+baileybeez@users.noreply.github.com>
2026-08-04 17:44:00 -0400

    introduced kAssert

--- src/kernel/arch/i686/cpu.h
+++ src/kernel/arch/i686/cpu.h
@@ -3,6 +3,8 @@
 
 #define kHalt()      for(;;) { __asm__ volatile("cli; hlt;"); }
 
+#define kEflags_IF   0x0200
+
 static inline void interruptsDisable() { __asm__ volatile("cli" ::: "memory"); }
 static inline void interruptsEnable()  { __asm__ volatile("sti" ::: "memory"); }
 
@@ -16,4 +18,11 @@ static inline u32 irqSave()
 static inline void irqRestore(u32 flags)
 {
    __asm__ volatile("push %0; popf" :: "r"(flags) : "memory", "cc");
+}
+
+static inline u32 readEflags()
+{
+   u32 flags;
+   __asm__ volatile("pushf; pop %0;" : "=r"(flags) :: "memory");
+   return flags;
 }
