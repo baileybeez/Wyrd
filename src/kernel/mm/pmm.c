0de0b33cada73c5edeaf750e4d3092933c31854b
BaileyBeez <2171091+baileybeez@users.noreply.github.com>
2026-08-04 22:25:29 -0400

    naming cleanup and FreeFrame() helper

--- src/kernel/mm/pmm.c
+++ src/kernel/mm/pmm.c
@@ -6,8 +6,8 @@
 
 typedef void (*fncFlagFrame)(u32 idx);
 
-static u8 g_frameBitmap[kFrameBitmapSize] = {0};
-static MemoryBitmap g_pmm;
+static u8 _frameBitmap[kFrameBitmapSize] = {0};
+static MemoryBitmap _bitmap;
 
 /*
  * frames are flagged in a bit mask (g_pmm.bitmap)
@@ -16,51 +16,51 @@ static MemoryBitmap g_pmm;
  */
 void _pmmClaimFrame(u32 frameIndex)
 {
-   if (frameIndex >= g_pmm.totalFrames)
+   if (frameIndex >= _bitmap.totalFrames)
       return;
 
    u32 idx = frameIndex >> 3; // frame / 8
    u32 bit = frameIndex &  7; // frame % 8
    u8 mask = 1 << bit;
-   if (g_pmm.bitmap[idx] & mask) {
+   if (_bitmap.bitmap[idx] & mask) {
       return;
    }
 
-   g_pmm.bitmap[idx] |= mask;
-   g_pmm.usedFrames++;
+   _bitmap.bitmap[idx] |= mask;
+   _bitmap.usedFrames++;
 }
 
 void _pmmReleaseFrame(u32 frameIndex)
 {
-   if (frameIndex >= g_pmm.totalFrames)
+   if (frameIndex >= _bitmap.totalFrames)
       return;
 
    u32 idx = frameIndex >> 3; // frame / 8
    u32 bit = frameIndex &  7; // frame % 8
    u8 mask = 1 << bit;
-   if (!(g_pmm.bitmap[idx] & mask)) {
+   if (!(_bitmap.bitmap[idx] & mask)) {
       return;
    }
 
-   g_pmm.bitmap[idx] &= ~mask;
-   g_pmm.usedFrames--;
+   _bitmap.bitmap[idx] &= ~mask;
+   _bitmap.usedFrames--;
 }
 
 u32 _pmmFindFreeFrame(u32 hint)
 {
    u32 frameIdx = hint;
-   while(frameIdx < g_pmm.totalFrames) {
+   while(frameIdx < _bitmap.totalFrames) {
       u32 idx  = frameIdx >> 3; // frame / 8
       u32 bit  = frameIdx &  7; // frame % 8
       u8  mask = 1 << bit;
-      if (!(g_pmm.bitmap[idx] & mask)) {
+      if (!(_bitmap.bitmap[idx] & mask)) {
          break;
       }
 
       frameIdx++;
    }
    
-   if (frameIdx >= g_pmm.totalFrames) {
+   if (frameIdx >= _bitmap.totalFrames) {
       return kInvalidFrame;
    }
 
@@ -96,14 +96,14 @@ void _pmmClaimAddrRange(u32 start, u32 end)
 
 void pmmInit(BootInfo* info)
 {
-   g_pmm.totalFrames = info->totalSystemRam / kFrameSize;
-   g_pmm.usedFrames  = g_pmm.totalFrames;
-   g_pmm.bitmap      = g_frameBitmap;
-   g_pmm.bitmapSize  = kFrameBitmapSize;
-   g_pmm.lastHint    = 0;
+   _bitmap.totalFrames = info->totalSystemRam / kFrameSize;
+   _bitmap.usedFrames  = _bitmap.totalFrames;
+   _bitmap.bitmap      = _frameBitmap;
+   _bitmap.bitmapSize  = kFrameBitmapSize;
+   _bitmap.lastHint    = 0;
 
    // set all frames as claimed, then work through mmap entries and release available space
-   memset(g_pmm.bitmap, 0xFF, kFrameBitmapSize);
+   memset(_bitmap.bitmap, 0xFF, kFrameBitmapSize);
    for (u32 i = 0; i < info->mmapEntryCount; i++) {
       if (info->mmapEntries[i].type == kMMapEntryType_Available) {
          u64 length   = kLowHighToU64(info->mmapEntries[i].lengthHigh, info->mmapEntries[i].lengthLow);
@@ -129,8 +129,8 @@ void pmmInit(BootInfo* info)
 
 u32  pmmAllocFrame()
 {
-   u32 frameIdx = _pmmFindFreeFrame(g_pmm.lastHint);
-   if (frameIdx == kInvalidFrame && g_pmm.lastHint != 0) {
+   u32 frameIdx = _pmmFindFreeFrame(_bitmap.lastHint);
+   if (frameIdx == kInvalidFrame && _bitmap.lastHint != 0) {
       kTrace("last hint failed, trying again from 0");
       frameIdx = _pmmFindFreeFrame(0);
    }   
@@ -138,7 +138,7 @@ u32  pmmAllocFrame()
       return kInvalidFrame;
 
    _pmmClaimFrame(frameIdx); 
-   g_pmm.lastHint = frameIdx + 1;
+   _bitmap.lastHint = frameIdx + 1;
    return frameIdx * kFrameSize;
 }
 
@@ -147,15 +147,20 @@ void pmmFreeFrame(u32 addr)
    _pmmReleaseFrame(addr / kFrameSize);
 }
 
+u32  pmmFreeFrames()
+{
+   return _bitmap.totalFrames - _bitmap.usedFrames;
+}
+
 void pmmDumpStats()
 {
-   u32 free = g_pmm.totalFrames - g_pmm.usedFrames;
-   kTrace("*** PMM ***  Total: %u, Used: %u, Free: %u", g_pmm.totalFrames, g_pmm.usedFrames, free);
-   kTrace("*** PMM ***  Last Hint: %u", g_pmm.lastHint);
+   u32 free = _bitmap.totalFrames - _bitmap.usedFrames;
+   kTrace("*** PMM ***  Total: %u, Used: %u, Free: %u", _bitmap.totalFrames, _bitmap.usedFrames, free);
+   kTrace("*** PMM ***  Last Hint: %u", _bitmap.lastHint);
 
    u8 map[8] = {0};
    for (u32 i = 0; i < 8; i++)
-      map[i] = g_pmm.bitmap[i];
+      map[i] = _bitmap.bitmap[i];
 
    kTrace("*** PMM ***  %x %x %x %x %x %x %x %x", 
       map[0], map[1], map[2], map[3], map[4], map[5], map[6], map[7]);
