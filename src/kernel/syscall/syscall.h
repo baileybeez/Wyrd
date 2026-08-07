c1270aaf2f1cc4f8e1ccacdba78c316a86862fdf
BaileyBeez <2171091+baileybeez@users.noreply.github.com>
2026-08-06 21:43:07 -0400

    updated sysWrite to accept fd for arg0

--- src/kernel/syscall/syscall.h
+++ src/kernel/syscall/syscall.h
@@ -1,15 +1,6 @@
 #pragma once
 #include "wyrd.h"
-
-#define kSysErr_Fault   -1
-#define kSysErr_TooLong -2
-#define kSysErr_NoSys   -3
-
-#define kSys_Read       0
-#define kSys_Write      1
-#define kSys_Exit       2
-
-#define kSyscallCount   3
+#include "sys.h"
 
 typedef i32 (*fncSyscall)(u32 a0, u32 a1, u32 a2);
 
